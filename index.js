9ac3d6c47381ae862fae54337bd05ad6e9dee482
Oppai-San <69411208+Oppai-San@users.noreply.github.com>
2020-08-27 11:44:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("index.js has been executed");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("NzQxODczODc0MTY1MzAxMzI4.Xy954g.LyWgfcsqcOAD6-myBgWxPowfaig");
+client.login("NzQxODczODc0MTY1MzAxMzI4.Xy954g.Vm1KsMTSqWr-etsZU0bRKScwxV8");
 
 client.on("ready", () => {
   console.log("bot has logged into the account");
